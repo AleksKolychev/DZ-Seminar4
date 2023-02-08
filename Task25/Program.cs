7b1190a180722e854d890afba52cc128f27310ae
AleksKolychev <aleksushome@gmail.com>
2023-02-08 11:48:32 +0500

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите показатель степени: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double Result = n;
+for (int i = 1; i < m; i++)
+    Result = n * Result;
+Console.WriteLine(Result);
