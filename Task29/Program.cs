a7cbd7498b5628c3d37222a8ae887f9e46fe731e
AleksKolychev <aleksushome@gmail.com>
2023-02-11 12:37:15 +0500

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+for (int i = 0; i < n; i ++)
+    array[i] = new Random().Next(-1000, 1001);
+Console.WriteLine($"[{string.Join(" ", array)}]");
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+foreach (int element in array)
+{
+    if (element == x)
+    count ++;
+}
+Console.WriteLine($"Результат: {count}");
