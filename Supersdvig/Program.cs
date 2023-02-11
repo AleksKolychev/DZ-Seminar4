d25ca7165da9baf3bdccb9fb470568d5f06b690c
AleksKolychev <aleksushome@gmail.com>
2023-02-11 13:18:44 +0500

    Add Supersdvig

---
+++ Supersdvig/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arrayFirst = new int[n];
+int[] arrayResult = new int[n];
+for (int i = 0; i < n; i++)
+    arrayFirst[i] = new Random().Next(1, 11);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", arrayFirst)}]");
+Console.WriteLine("Введите число сдвига: ");
+int k = Convert.ToInt32(Console.ReadLine());
+k = k % n;
+if (k > 0)
+{
+    for (int i = 0; i < k; i++)
+        arrayResult[i] = arrayFirst[n - k + i];
+    for (int i = 0; i < n - k; i++)
+        arrayResult[k + i] = arrayFirst[i];
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", arrayResult)}]");
+}
+else
+{
+    k = (-1) * k;
+    for (int i = 0; i < k; i++)
+        arrayResult[n - k + i] = arrayFirst[i];
+    for (int i = 0; i < n - k; i++)
+        arrayResult[i] = arrayFirst[k + i];
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", arrayResult)}]");
+}
